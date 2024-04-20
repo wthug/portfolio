da0a4d5f4fa60c43f819272834dd5d57510c3259
wthug <gdscutsav@gmail.com>
2024-04-20 23:34:25 +0530

    commiting index file

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import routes from './routes/routes.js'
+import path from 'path'
+const app =express();
+const port = process.env.PORT || 3000
+
+//setting up for statoc files
+app.use(express.static(path.join(process.cwd(),'public')));
+
+//setting up my ejs
+app.set('view engine' , 'ejs');
+app.set('views' , './views');
+
+
+// creating route
+app.use('/',routes)
+// app.get('/',(req,res)=>{
+//     res.send("welcome to home ,tihs is my portfolio")
+// })
+
+app.listen(port, ()=>{
+    console.log(`server is running on port ${port}`)
+})
